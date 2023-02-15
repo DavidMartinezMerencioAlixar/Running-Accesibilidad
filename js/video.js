bfe3aa8cbaf525d6c163deb8db40f38c48719327
DavidMartinezMerencioAlixar <alum.dmartinezm.2022@iesalixar.org>
2023-02-15 17:43:51 +0100

    Cambios en vídeo inicial

--- js/video.js
+++ js/video.js
@@ -8,3 +8,20 @@ botonCerrarVideo.addEventListener("click", () => {
     document.getElementById("divVideo").classList.add("d-none");
     document.getElementById("divPaginaPrincipal").classList.remove("d-none");
 });
+
+let backgroundVideo = document.getElementById("background-video");
+let miVideo = document.getElementById("miVideo");
+
+window.addEventListener("load", () => {
+    miVideo.addEventListener("play", () => {
+        backgroundVideo.play();
+    });
+
+    miVideo.addEventListener("pause", () => {
+        backgroundVideo.pause();
+    });
+
+    miVideo.addEventListener("ended", () => {
+        backgroundVideo.play();
+    });
+});
